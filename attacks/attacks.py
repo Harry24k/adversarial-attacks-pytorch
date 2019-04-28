a798b14142ab647199982bb69f54bfec3e0d6567
Harry Kim <41545927+HarryK24@users.noreply.github.com>
2019-04-28 14:25:50 +0900

    ver 0.2
    Save function is now providing accuaracy as well.

--- attacks/attacks.py
+++ attacks/attacks.py
@@ -5,6 +5,7 @@ Written by Harry Kim
 Licensed under the MIT License
 
 WARNING :: All images should be scaled to [0, 1] with transform[to.Tensor()] before used in attacks.
+WARNING :: All models should return ONLY ONE vector of `(N, C)` where `C = number of classes`.
 """
 
 import torch
@@ -37,25 +38,96 @@ class Attacks(object):
     
     # Save image data as torch tensor from data_loader
     # If you want to reduce the space of dataset, set 'to_unit8' as True
-    def save(self, file_name, data_loader, to_uint8 = True):
+    # If you don't want to know about accuaracy of the model, set accuracy as False
+    def save(self, file_name, data_loader, to_uint8 = True, accuracy = True):
         image_list = []
         label_list = []
         
-        for images, labels in data_loader :
+        correct = 0
+        total = 0
+        
+        total_batch = len(data_loader)
+        
+        for step, (images, labels) in enumerate(data_loader) :
             
             adv_images = self.__call__(images, labels)
             
+            if accuracy :
+                outputs = self.model(adv_images)
+
+                _, predicted = torch.max(outputs.data, 1)
+
+                total += labels.size(0)
+                correct += (predicted == labels.to(self.device)).sum()
+
             if to_uint8 :
                 image_list.append((adv_images*255).type(torch.uint8).cpu())
             else :
                 image_list.append(adv_images.cpu())
+                
             label_list.append(labels)
+        
+            print('- Save Progress : %2.2f %%        ' %((step+1)/total_batch*100), end='\r')
+        
+        if accuracy :
+            acc = 100 * float(correct) / total
+            print('\n- Accuracy of the model : %f %%' % (acc), end='')
         
         x = torch.cat(image_list, 0)
         y = torch.cat(label_list, 0)
         
         torch.save((x, y), file_name)
         
+        print('\n- Save Complete!')
+        
+    # Load image data as torch dataset
+    # When scale=True it automatically tansforms images to [0, 1]
+    def load(self, file_name, scale = True) :
+        adv_images, adv_labels = torch.load(file_name)
+        
+        if scale :
+            adv_data = torch.utils.data.TensorDataset(adv_images.float() / adv_images.max(), adv_labels)
+        else :
+            adv_data = torch.utils.data.TensorDataset(adv_images.float(), adv_labels)
+            
+        return adv_data
+    
+    ########################################## DEPRECIATED ##########################################
+    '''
+    # Evaluate accuaracy of a model
+    # With default 'model = None', it will return accuracy of white box attack
+    # If not, it will return accuracy of black box attack with self.model as holdout model
+    
+    def eval(self, data_loader, model = None) :
+        
+        if model is None :
+            model = self.model
+        else :
+            model = model.eval()
+
+        correct = 0
+        total = 0
+
+        total_batch = len(data_loader)
+        
+        for step, (images, labels) in enumerate(data_loader) :
+
+            adv_images = self.__call__(images, labels)
+            outputs = model(adv_images)
+
+            _, predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels.to(self.device)).sum()
+            
+            print('- Evaluation Progress : %2.2f %%        ' %((step+1)/total_batch*100), end='\r')
+
+        accuracy = 100 * float(correct) / total
+        print('\n- Accuracy of model : %f %%' % (accuracy))
+
+        return accuracy
+    '''
+    
 class FGSM(Attacks):
     """
     FGSM attack in the paper 'Explaining and harnessing adversarial examples'
