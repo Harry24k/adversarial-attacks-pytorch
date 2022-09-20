0486ce8015b152625d58261c16737da52f19d425
Harry Kim <24K.Harry@gmail.com>
2022-09-20 23:19:53 +0900

    Add coverage

--- code_coverage/test_.py
+++ code_coverage/test_.py
@@ -1,2 +1,17 @@
-def test_duet_sf():
+import sys
+sys.path.insert(0, '..')
+
+
+def import_torchattacks():
+    import torchattacks
+    print(torchattacks.__version__)
+    assert True
+
+
+def load_model():
+    from robustbench.data import load_cifar10
+    # from robustbench.utils import load_model, clean_accuracy
+
+    images, labels = load_cifar10(n_examples=10)
+    device = "cpu"
     assert True
