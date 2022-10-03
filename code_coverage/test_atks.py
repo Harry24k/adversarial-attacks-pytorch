7b95a7189e0fd270b50fcba751d20980e2aaef53
Harry Kim <24K.Harry@gmail.com>
2022-10-03 20:06:05 +0900

    Update coverage

--- code_coverage/test_atks.py
+++ code_coverage/test_atks.py
@@ -4,17 +4,21 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import time
 
+import torch
 import torchattacks
 from robustbench.data import load_cifar10
 from robustbench.utils import load_model, clean_accuracy
 
+
 def get_model(model_name='Standard', device="cpu", model_dir='./models'):
     model = load_model(model_name, model_dir=model_dir, norm='Linf')
     return model.to(device)
-    
+
+
 def get_data(data_name='CIFAR10', device="cpu", n_examples=5, data_dir='./data'):
     images, labels = load_cifar10(n_examples=n_examples, data_dir=data_dir)
     return images.to(device), labels.to(device)
+
 
 def test_atks_on_cifar10(device="cpu", n_examples=5, model_dir='./models', data_dir='./data'):
     model = get_model(device=device, model_dir=model_dir)
@@ -22,14 +26,18 @@ def test_atks_on_cifar10(device="cpu", n_examples=5, model_dir='./models', data_
     clean_acc = clean_accuracy(model, images, labels)
 
     for atk_class in torchattacks.__all__:
-        if atk_class in  torchattacks.__wrapper__:
+        if atk_class in torchattacks.__wrapper__:
             continue
-        atk = eval("torchattacks."+atk_class)(model)
-        start = time.time()
-        adv_images = atk(images, labels)
-        end = time.time()
-        robust_acc = clean_accuracy(model, adv_images, labels)
-        sec = float(end - start) % 60
-        print('{0:<12}: clean_acc={1:2.2f} robust_acc={2:2.2f} sec={3:2.2f}'.format(atk_class, clean_acc, robust_acc, sec))
+        try:
+            atk = eval("torchattacks."+atk_class)(model)
+            start = time.time()
+            adv_images = atk(images, labels)
+            end = time.time()
+            robust_acc = clean_accuracy(model, adv_images, labels)
+            sec = float(end - start) % 60
+            print('{0:<12}: clean_acc={1:2.2f} robust_acc={2:2.2f} sec={3:2.2f}'.format(atk_class, clean_acc, robust_acc, sec))
+        except Exception as e:
+            print('{0:<12} ocurrs Error'.format(atk_class))
+            print(e)
 
     assert clean_acc >= robust_acc
--- code_coverage/test_import.py
+++ code_coverage/test_import.py
@@ -4,6 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import torchattacks
 
+
 def test_import_version():
     print(torchattacks.__version__)
     assert True
