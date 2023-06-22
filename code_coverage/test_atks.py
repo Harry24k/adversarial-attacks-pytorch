4245e76190072328d52e5348f2e543146f212ecf
Riko <xxy1836@gmail.com>
2023-06-22 16:25:43 +0800

    Can not fix auto test error...

--- code_coverage/test_atks.py
+++ code_coverage/test_atks.py
@@ -1,24 +1,25 @@
+from robustbench.utils import load_model
+from robustbench.utils import clean_accuracy
+from robustbench.data import load_cifar10
 import torchattacks
 import torch
 import pytest
 import time
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
-
 
 CACHE = {}
 
 
 def get_model(model_name='Standard', device="cpu", model_dir='./models'):
-    from robustbench.utils import load_model
     model = load_model(model_name, model_dir=model_dir, norm='Linf')
     return model.to(device)
 
 
 def get_data(data_name='CIFAR10', device="cpu", n_examples=5, data_dir='./data'):
-    from robustbench.data import load_cifar10
     images, labels = load_cifar10(n_examples=n_examples, data_dir=data_dir)
     return images.to(device), labels.to(device)
 
@@ -26,9 +27,7 @@ def get_data(data_name='CIFAR10', device="cpu", n_examples=5, data_dir='./data')
 @torch.no_grad()
 @pytest.mark.parametrize("atk_class", [atk_class for atk_class in torchattacks.__all__ if atk_class not in torchattacks.__wrapper__])
 def test_atks_on_cifar10(atk_class, device="cpu", n_examples=5, model_dir='./models', data_dir='./data'):
-    print('>>>> '.format(os.getcwd()))
 
-    from robustbench.utils import clean_accuracy
     if CACHE.get('model') is None:
         model = get_model(device=device, model_dir=model_dir)
         CACHE['model'] = model
