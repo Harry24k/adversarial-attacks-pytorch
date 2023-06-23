fdae6c49a54af7579f3a9683f8dd37b99c7d4064
Riko <xxy1836@gmail.com>
2023-06-23 10:08:52 +0800

    Fix auto test device error

--- code_coverage/test_atks.py
+++ code_coverage/test_atks.py
@@ -52,10 +52,10 @@ def test_atks_on_cifar10(atk_class, device="cpu", n_examples=5, model_dir='./mod
         clean_acc = CACHE['clean_acc']
 
     try:
-        kargs = {'device': device}
+        kargs = {}
         if atk_class in ['SPSA']:
             kargs['max_batch_size'] = 5
-        atk = eval("torchattacks."+atk_class)(model, **kargs)
+        atk = eval("torchattacks."+atk_class)(model, device, **kargs)
         start = time.time()
         with torch.enable_grad():
             adv_images = atk(images, labels)
