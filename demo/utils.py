139af17badf7b891d0ab419b27fe36d19b91c03d
Harry Kim <24K.Harry@gmail.com>
2023-10-20 15:32:14 +0900

    Update utils.py

--- demo/utils.py
+++ demo/utils.py
@@ -67,3 +67,33 @@ def l2_distance(model, images, adv_images, labels, device="cuda"):
     delta = (adv_images - images.to(device)).view(len(images), -1)
     l2 = torch.norm(delta[~corrects], p=2, dim=1).mean()
     return l2
+
+
+@torch.no_grad()
+def get_accuracy(model, data_loader, atk=None, n_limit=1e10, device=None):
+    model = model.eval()
+
+    if device is None:
+        device = next(model.parameters()).device
+
+    correct = 0
+    total = 0
+
+    for images, labels in data_loader:
+
+        X = images.to(device)
+        Y = labels.to(device)
+
+        if atk:
+            X = atk(X, Y)
+
+        pre = model(X)
+
+        _, pre = torch.max(pre.data, 1)
+        total += pre.size(0)
+        correct += (pre == Y).sum()
+
+        if total > n_limit:
+            break
+
+    return (100 * float(correct) / total)
